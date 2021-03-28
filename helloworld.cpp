c486009cb6ee377515db81546eeeb76ba5c37bd6
jzumbrink <j.zumbrink@gmx.de>
2021-03-28 11:35:21 +0200

    sphere added

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,21 +5,30 @@
 #include <iostream>
 
 bool hit_sphere(const point3& center, double radius, const ray& r){
-    
+    vec3 oc = r.origin() - center;
+    auto a  = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
 }
 
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)){
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t * color(1.0, 1.0, 0.1);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
 
     // Image
-    const auto aspect_ratio = 16/9;
-    const int image_width = 400;
+    const auto aspect_ratio = 16.0/9.0;
+    const int image_width = 16;
     const int image_height = image_width/aspect_ratio;
+    std::cerr << image_height << std::endl;
 
     //Camera
     auto viewport_height = 2.0;
