10e3287c6e228753b7a57571c82ee5dbc71ca53c
jzumbrink <j.zumbrink@gmx.de>
2021-03-28 18:28:26 +0200

    diffused materials

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,10 +7,16 @@
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+
+    if (depth<=0)
+        return color(0, 0, 0);
+
+    if (world.hit(r, 0.001, infinity, rec)) {
+        //point3 target = rec.p + rec.normal + random_unit_vector();
+        point3 target = rec.p + random_in_hemisphere(rec.normal);
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
@@ -24,12 +30,13 @@ int main() {
     const int image_width = 400;
     const int image_height = image_width/aspect_ratio;
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     std::cerr << image_height << std::endl;
 
     //World 
     hittable_list world;
-    //world.add(make_shared<sphere>(point3(-1, -1, -1), 2));
+    world.add(make_shared<sphere>(point3(-1, -1, -1), 0.9));
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(1, 0.1, -1), 0.1));
     world.add(make_shared<sphere>(point3(-1, 0.1, -1), 0.3));
@@ -49,11 +56,17 @@ int main() {
                 auto u = (i+random_double()) / (image_width-1);
                 auto v = (j+random_double()) / (image_height-1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
+
+    for (size_t i = 0; i < 100; i++)
+    {
+        std::cerr << random_double() << "\n";
+    }
+    
 
     std::cerr << "\nDone.\n";
 
