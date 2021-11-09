ccaa914d8f9323328e8aebb24af417cfaca8dd6d
jzumbrink <j.zumbrink@gmx.de>
2021-11-09 16:35:38 +0100

    update vec3

--- vec3.h
+++ vec3.h
@@ -43,8 +43,6 @@ class vec3 {
         double length() const {
             return sqrt(length_squared());
         }
-<<<<<<< HEAD
-=======
 
         inline static vec3 random() {
         return vec3(random_double(), random_double(), random_double());
@@ -53,7 +51,6 @@ class vec3 {
         inline static vec3 random(double min, double max) {
             return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
         }
->>>>>>> developing
 };
 
 using point3 = vec3;
@@ -103,8 +100,6 @@ inline vec3 unit_vector(vec3 v){
     return v / v.length();
 }
 
-<<<<<<< HEAD
-=======
 vec3 random_in_unit_sphere() {
     while (true) {
         auto p = vec3::random(-1,1);
@@ -125,5 +120,4 @@ vec3 random_in_hemisphere(const vec3& normal) {
         return -in_unit_sphere;
 }
 
->>>>>>> developing
 #endif
